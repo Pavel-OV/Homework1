cdf534118e694efaee00119f25075833f45d869b
Pavel <black_yaguar@mail.ru>
2022-11-26 09:47:04 +0500

    the second homework task

---
+++ task002/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход три числа и 
+выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите  числа Сравните их и напишите какое больше ");
+Console.Write("Придумайте число а=");
+int number_a = int.Parse(Console.ReadLine());
+Console.Write("Ещё раз придумайте чилсо b=");  //Здесь можно придумать проверку, на одинаковые числа
+int number_b = int.Parse(Console.ReadLine());
+Console.Write("Придумайте число c=");
+int number_c = int.Parse(Console.ReadLine());
+
+if (number_a > number_b & number_a > number_c){
+    Console.WriteLine("Число а больше вeх");
+    Console.Write(number_a);
+}
+if (number_a < number_b & number_b > number_c){
+    Console.WriteLine("Число b больше вeх");
+    Console.Write(number_b);
+}
+if (number_c > number_a & number_b < number_c){
+    Console.WriteLine("Число c больше вeх");
+    Console.Write(number_c);
+}
